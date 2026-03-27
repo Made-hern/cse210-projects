6e375a26c354886aaf2371ea858e8af64cf20ee0
Made-hern <madelinrebecahernandezaguilar@gmail.com>
2026-03-27 12:28:07 -0600

    finished YouTube video program whith screenshot

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        // Video 1
+        Video video1 = new Video("How to Cook Pasta", "Chef Maria", 300);
+        video1.AddComment(new Comment("John", "Great recipe!"));
+        video1.AddComment(new Comment("Ana", "Loved it!"));
+        video1.AddComment(new Comment("Luis", "Very helpful"));
+        videos.Add(video1);
+
+        // Video 2
+        Video video2 = new Video("C# Basics Tutorial", "CodeMaster", 600);
+        video2.AddComment(new Comment("Carlos", "Easy to understand"));
+        video2.AddComment(new Comment("Sofia", "Thanks for this!"));
+        video2.AddComment(new Comment("Miguel", "Very clear explanation"));
+        videos.Add(video2);
+
+        // Video 3
+        Video video3 = new Video("Workout at Home", "FitLife", 900);
+        video3.AddComment(new Comment("Laura", "Great workout"));
+        video3.AddComment(new Comment("Pedro", "I feel stronger already"));
+        video3.AddComment(new Comment("Diana", "Awesome routine"));
+        videos.Add(video3);
+
+        // Mostrar información
+        foreach (Video video in videos)
+        {
+            video.DisplayVideoInfo();
+        }
     }
 }
---
+++ week04/YouTubeVideos/video.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+
+    private List<Comment> comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+
+        Console.WriteLine("Comments:");
+        foreach (Comment comment in comments)
+        {
+            Console.WriteLine($"- {comment.Author}: {comment.Text}");
+        }
+
+        Console.WriteLine();
+    }
+}
